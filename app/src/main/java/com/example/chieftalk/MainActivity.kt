7050b7c910683beedfb0916f4f07f21d2fda71c1
tr00per <kolosovskiy91@gmail.com>
2022-10-03 17:22:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chieftalk/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.chieftalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chieftalk.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
