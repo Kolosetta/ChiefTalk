2bdda15de6a09c0f0ff0ecee6764a06a4726037c
tr00per <kolosovskiy91@gmail.com>
2022-10-08 23:48:36 +0300

    Начата разработка фрагмента воода номера для регистрации

---
+++ app/src/main/java/com/example/chieftalk/activities/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.example.chieftalk.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chieftalk.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/example/chieftalk/ui/fragments/EnterPhoneFragment.kt
@@ -0,0 +1,28 @@
+package com.example.chieftalk.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.chieftalk.R
+import com.example.chieftalk.databinding.FragmentEnterPhoneBinding
+
+
+class EnterPhoneFragment : Fragment() {
+
+    private lateinit var binding: FragmentEnterPhoneBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEnterPhoneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = EnterPhoneFragment()
+    }
+}
